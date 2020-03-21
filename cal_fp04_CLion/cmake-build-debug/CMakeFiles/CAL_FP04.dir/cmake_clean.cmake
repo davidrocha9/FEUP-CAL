378016f4fe43a44360743600640713ea03a2464a
davidrocha9 <up201806371@fe.up.pt>
2020-03-21 11:38:30 +0000

    Added TP4

---
+++ cal_fp04_CLion/cmake-build-debug/CMakeFiles/CAL_FP04.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CAL_FP04.exe"
+  "CAL_FP04.exe.manifest"
+  "CAL_FP04.pdb"
+  "CMakeFiles/CAL_FP04.dir/Tests/Person.cpp.obj"
+  "CMakeFiles/CAL_FP04.dir/Tests/tests.cpp.obj"
+  "CMakeFiles/CAL_FP04.dir/main.cpp.obj"
+  "libCAL_FP04.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CAL_FP04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
